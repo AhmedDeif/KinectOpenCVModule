b917ccd42887b49fbdce7b35a98a9b828b504442
AhmedDeif <ahmed.adel.abodeif@gmail.com>
2016-04-22 12:03:19 +0200

    fixing memory leak

--- KinectV1/main.cpp
+++ KinectV1/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
 	KinectV1Sensor kinectSensor = KinectV1Sensor();
-	if(SUCCEEDED(kinectSensor.CreateFirstConnected()))
+	if (SUCCEEDED(kinectSensor.CreateFirstConnected()))
 	{
 		Mat color = Mat(480, 640, CV_8UC3);
 		Mat depth = Mat(480, 640, CV_16U);
@@ -13,12 +13,12 @@ int main()
 			MsgWaitForMultipleObjects(kinectSensor.eventCount, kinectSensor.hEvents, FALSE, INFINITE, QS_ALLINPUT);
 			if (WAIT_OBJECT_0 == WaitForSingleObject(kinectSensor.m_hNextColorFrameEvent, 0))
 			{
-				kinectSensor.getRGBFrame(&color);
+				kinectSensor.getRGBFrame(color);
 
 			}
 			if (WAIT_OBJECT_0 == WaitForSingleObject(kinectSensor.m_hNextDepthFrameEvent, 0))
 			{
-				kinectSensor.getDepthFrame(&depth);
+				kinectSensor.getDepthFrame(depth);
 			}
 			imshow("ColorStream", color);
 			imshow("DepthStream", depth);
